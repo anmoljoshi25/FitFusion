648f786906251ebb07c9349beee387a9af0091f7
anmoljoshi25 <joshianmol2023@gmail.com>
2025-03-16 22:52:47 +0530

    gsap add

--- app.js
+++ app.js
@@ -127,3 +127,16 @@ main.addEventListener('mousemove',function(dets){
 })
 
 
+gsap.to(".page1 h1", {
+    transform: "translateX(-100%)",
+fontWeight: 100,
+scrollTrigger: {
+    trigger: ".page1",
+    scroller: "body",
+    // markers: true,
+    start: "top 0",
+    end: "top -200%",
+    scrub: 2,
+    pin: true
+}
+});
